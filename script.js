c1e7f1b2b39753f2178befd6ad5a8fd90f0fd983
Andy <badzju@gmail.com>
2020-05-20 20:50:30 +0300

    ДЗ2

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-const numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели ?');
+/* Задание на урок:
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять. (К любой строке можно обратиться как 
+str.length - и получить её длину)
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+"use strict";
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
-    count: 'numberOfFilms',
+    count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
@@ -9,9 +22,8 @@ const personalMovieDB = {
 };
 
 for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов', ''),
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
           b = prompt('На сколько оцените его?', '');
-
 
     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
         personalMovieDB.movies[a] = b;
@@ -20,32 +32,16 @@ for (let i = 0; i < 2; i++) {
         console.log('error');
         i--;
     }
-
 }
 
-
 if (personalMovieDB.count < 10) {
-    console.log('Просмотрено довольно мало фильмов');
+    console.log("Просмотрено довольно мало фильмов");
 } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log('Вы классический зритель');
+    console.log("Вы классический зритель");
 } else if (personalMovieDB.count >= 30) {
-    console.log('Вы киноман');
+    console.log("Вы киноман");
 } else {
-    console.log('Произошла ошибка');
+    console.log("Произошла ошибка");
 }
 
 console.log(personalMovieDB);
-
-
-
-
-
-
-/*const a = prompt('Один из последних просмотренных фильмов', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов', ''),
-      d = prompt('На сколько оцените его?', '');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-*/
